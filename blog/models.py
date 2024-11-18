8ac1c1f9e6230dd85ee22838d66d20997c107708
woodoo_bti <allance12@mail.ru>
2024-11-18 11:39:08 +0300

    *creating 2 views *updating ulrs *create base-page and 2 templ

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
@@ -21,7 +27,7 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2,
                               choices=Status.choices,
-                              default=Status.DRAFT)
+                              default=Status.PUBLISHED)
 
     class Meta:
         ordering = ['-publish']
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 def post_list(request):
-    return render(request, 'blog/index.html', context={
-        'title' : 'Post Lists',
-        'content' : 'Posts: '
-    })
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post,
+                             id=id, status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
